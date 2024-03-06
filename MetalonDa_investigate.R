d0ca10b3e51170b36b1b2131bc9643930c106489
Sam Windle <sam@windle.me.uk>
2024-03-06 11:13:19 +0000

    Got metalondaALL running permuations removed redunant code

--- MetalonDa_investigate.R
+++ MetalonDa_investigate.R
@@ -3,68 +3,20 @@ library(readr)
 library(dplyr)
 
 #Load data
-Bracken.table <- readr::read_tsv("./bracken_db1.tsv")
+Bracken.table <- as.data.frame(readr::read_tsv("./bracken_db1.tsv"))
 rownames(Bracken.table) <- Bracken.table$taxonomy_id
 Bracken.table$"taxonomy_id" <- NULL
 meta.table <- read.csv("./data/3in1FromMetadata.csv")
 long.meta <- subset(meta.table, Study == "Longitudinal ")
 
-#Sorting for each group (corrral)
 
-
-get.samples.for.group <- function(group, meta.table){
-  group.table <- subset(meta.table, Corral.ID == group)
-  group.table <- select(group.table, "LabID", "CGR.sample.ID")
-  rownames(group.table) <- group.table$LabID
-  group.table$"LabID" <- NULL
-  return(group.table)
-}
-
-rename.samples <- function(sample.data){
-  #print(sample.data)
-  renamed <- c()
-  for (index in rownames(sample.data)){
-    # For loop takes the index of the sample.data and gets the LabID and CGR.sample.ID,
-    # then renames the file to the format "CGR.sample.ID-LabID_pe_CGR.sample.ID-LabID_db1.bracken"
-    # fitting the outputs for taxprofiler. 
-    #print(paste("index value:", index))
-    sample.ID <- index
-    CGR.ID <- sample.data[index,"CGR.sample.ID"]
-    #print(paste("name", index, "ID:", CGR.ID))
-    taxprofiler.output.name <- paste0(CGR.ID,"-",sample.ID,"_pe_",CGR.ID,"-", sample.ID, "_db1.bracken")
-    renamed <- c(renamed, taxprofiler.output.name)
-  }
-  return(renamed)
-}
+# get vectors functions
 
 count.vector <- function(sample.data){
   return(length(sample.data))
 }
 
-get.timepoints <- function(corral, meta.table){
-  dataframe <- subset(meta.table, Corral.ID == corral)
-  timepoints <- dataframe[["Day"]]
-  return(timepoints)
-}
 
-
-# running functions
-
-c20.samples <- rename.samples(get.samples.for.group(20, long.meta))
-c20.count <- count.vector(c20.samples)
-c20.timepoints <- get.timepoints(20, long.meta)
-print(c20.timepoints)
-print(count.vector(c20.timepoints))
-
-c21.samples <- rename.samples(get.samples.for.group(21, long.meta))
-c23.samples <- rename.samples(get.samples.for.group(23, long.meta))
-print(c20.samples)
-print(c20.count)
-
-print(c21.samples)
-print(c23.samples)
-
-## Attempt 2 at getting vectors
 
 get.group <- function(meta.table, LabID){
   group <- meta.table[LabID, "Corral.ID"]
@@ -76,6 +28,8 @@ get.group <- function(meta.table, LabID){
     return("control")
   }
 }
+
+
 
 
 get.vectors <- function(count.table, meta.table, count.index.columns=1){
@@ -104,6 +58,9 @@ get.vectors <- function(count.table, meta.table, count.index.columns=1){
   }
   return(list(group.vector = group, timepoint.vector = timepoints, ID.vector = ID))
 }
+
+
+# executing the get vectors functions 
 
 vectors <- get.vectors(Bracken.table, long.meta)
 print(vectors$ID.vector)
